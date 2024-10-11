133eb908241695076b28d768cdf17c3c15e75fbc
BryceI667 <bryceinouye285@gmail.com>
2024-10-10 18:33:02 -0700

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,14 @@
+console.log("nigger jew kike");
+
+let names = "aron yager";
+let body = "penis";
+
+console.log(names, body);
+
+const pie = 3.14;
+
+console.log(pie)
+
+let pussy = 0.7 - (1 + 2) * 5
+
+console.log(pussy)
